a7bc87daf9b27b4d0fb15bd570e9ccd99727d0a2
iki85 <riki.tinoza@hotmail.com>
2018-12-14 11:44:39 +0100

    index

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
   <section class="section is-large">
     <div class="container has-text-centered">
       <h1 class="title">Hi, I'm Riki Tnoza</h1>
-      <h2 class="subtitle">a Front-end web Developer based in Lagos, Nigeria.</h2>
+      <h2 class="subtitle">a Front-end web Developer based in Paris, France.</h2>
     </div>
   </section>
 </template>
